3a8ce28737775cf5989c28e5fd339a6867befa5a
H-WT <112571331+H-WT@users.noreply.github.com>
2023-12-27 12:52:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/Zxilly/UA2F package/UA2F
-git clone https://github.com/lucikap/luci-app-ua2f package/luci-app-ua2f
+#git clone https://github.com/Zxilly/UA2F package/UA2F
+#git clone https://github.com/lucikap/luci-app-ua2f package/luci-app-ua2f
 
 ## 非R4A从这里开始到最后都不要
 ### 修改为R4A千兆版Breed直刷版
